947d72e93f9eb58287e0ba999150332a406877f6
rjtprasad <prasadrajat28@gmail.com>
2025-01-19 23:47:48 +0530

    Authentication using Clerk

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,19 +2,22 @@ import BreadcrumbHeader from "@/components/BreadcrumbHeader";
 import DesktopSidebar from "@/components/Sidebar";
 import { ModeToggle } from "@/components/ThemeModeToggle";
 import { Separator } from "@/components/ui/separator";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-screen">
-        <DesktopSidebar />
+      <DesktopSidebar />
       <div className="flex flex-col flex-1 min-h-screen">
         <header className="flex items-center justify-between px-6 py-4 h-[50px] container">
           <BreadcrumbHeader />
-          <div className="gap-1 flex items-center">
+          <div className="gap-5 flex items-center">
             <ModeToggle />
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
           </div>
-
         </header>
         <Separator />
         <div className="overflow-auto">
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,14 +6,11 @@ const NotFoundPage = () => {
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
       <div className="text-center">
         <h1 className="text-6xl font-bold text-primary mb-4">404</h1>
-
         <h2 className="text-2xl font-semibold mb-4">Page Not Found</h2>
-
         <p className="text-muted-foreground mb-8 max-w-md">
           Don&apos;t worry, even the best data sometimes gets lost in the
           internet.
         </p>
-
         <div className="flex flex-col sm:flex-row justify-center gap-4">
           <Link
             href={"/"}
@@ -25,11 +22,9 @@ const NotFoundPage = () => {
           </Link>
         </div>
       </div>
-      <div className="mt-12 text-center">
-        <p className="text-sm text-muted-foreground">
-          If you believe this is an error, please contact our support team.
-        </p>
-      </div>
+      <footer className="mt-12 text-center text-sm text-muted-foreground">
+        If you believe this is an error, please contact our support team.
+      </footer>
     </div>
   );
 };
