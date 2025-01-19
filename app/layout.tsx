e382669d59ee0ae6c58b649afa46bf83f477109b
rjtprasad <prasadrajat28@gmail.com>
2025-01-19 22:09:10 +0530

    Dark Mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import AppProviders from "@/components/providers/AppProviders";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "ScrapeFlow",
-  description: "ScrapeFlow is a powerful SaaS application, allow users to visually build and execute web scraping workflows.",
+  description:
+    "ScrapeFlow is a powerful SaaS application, allow users to visually build and execute web scraping workflows.",
 };
 
 export default function RootLayout({
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AppProviders>{children}</AppProviders>
+      </body>
     </html>
   );
 }
